113c9e723b3fea4e5de4b32349c49142c0eaec13
juppak <419kog@gmail.com>
2022-11-03 11:26:23 +0000

    Fix custom_train

--- custom_hydra/custom_train.py
+++ custom_hydra/custom_train.py
@@ -14,12 +14,17 @@ from torch.utils.data import DataLoader
 ######## 주영 수정 #######
 # from sklearn.metrics import roc_auc_score
 from libauc.metrics import auc_roc_score
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from utils import *
 #########################
 from data_loader import dataset_CheXpert # Load our custom loader
 from data_loader.dataset_CheXpert import *
 
 best_val_roc_auc = 0
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 def train(dataloader, val_loader, model, loss_f, optimizer, cfg):
     size = len(dataloader.dataset)
     global best_val_roc_auc
@@ -36,13 +41,23 @@ def train(dataloader, val_loader, model, loss_f, optimizer, cfg):
         optimizer.step()
         if batch % 500 == 0:
             loss, current = loss.item(), batch * len(data_X)
-            val_loss, val_roc_auc = val(val_loader, model, loss_f)
+            ########### val_pred, val_true 추가 ##########
+            val_loss, val_roc_auc, val_pred, val_true = val(val_loader, model, loss_f)
+            ###############################################
             if best_val_roc_auc < val_roc_auc:
                 best_val_roc_auc = val_roc_auc
                 torch.save(model.state_dict(), cfg.ckpt_name)
                 print("Best model saved.")
+                ######### custom_metrics.py #######
+                custom_metrics(val_pred, val_true)
+                ###################################
             print(f"Batch ID: {batch}, loss: {loss:>7f}, val_loss = {val_loss:>7f}, val_roc_auc: {val_roc_auc:>4f}, Best_val_score: {best_val_roc_auc:>4f}, [{current:>5d}/{size:>5d}]")
+        #### 주영 수정 ###
+        model.train()
+        #################
         #tune.report(loss=val_loss, accuracy=val_roc_auc)
+
+
 def val(dataloader, model, loss_f):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -60,15 +75,19 @@ def val(dataloader, model, loss_f):
             val_loss += loss_f(pred, y).item()
             val_pred.append(pred.cpu().detach().numpy())
             val_true.append(y.cpu().numpy())
-    val_true = np.concatenate(val_true)
-    val_pred = np.concatenate(val_pred)
-    ########## 주영 수정 #########
-    auc_roc_scores = auc_roc_score(val_true, val_pred)
-    val_roc_auc = np.mean(auc_roc_scores)
-    ##############################
-    val_loss /= num_batches
-    #print(f"Batch ID: {idx}, Accuracy: {(100*correct):>0.1f}, loss: {loss:>7f}, [{current:>5d}/{size:>5d}], val_roc_auc: {val_roc_auc:>4f}, Best_val_score: {best_val_roc_auc:>4f}, [{current:>5d}/{size:>5d}]")
-    return val_loss, val_roc_auc
+        val_true = np.concatenate(val_true)
+        val_pred = np.concatenate(val_pred)
+        ########## 주영 수정 #########
+        auc_roc_scores = auc_roc_score(val_true, val_pred)
+        val_roc_auc = np.mean(auc_roc_scores)
+        ##############################
+        val_loss /= num_batches
+        #print(f"Batch ID: {idx}, Accuracy: {(100*correct):>0.1f}, loss: {loss:>7f}, [{current:>5d}/{size:>5d}], val_roc_auc: {val_roc_auc:>4f}, Best_val_score: {best_val_roc_auc:>4f}, [{current:>5d}/{size:>5d}]")
+        ############ val_pred, val_true 추가 ############
+    return val_loss, val_roc_auc, val_pred, val_true        
+    #################################################
+
+
 @hydra.main(
     version_base = None,
     config_path='config',
@@ -100,5 +119,7 @@ def trainval(cfg: DictConfig):
     #     path = os.path.join(checkpoint_dir, "checkpoint")
     #     torch.save((model.state_dict(), optimizer.state_dict()), path)
     print("Done!")
+
+    
 if __name__ == "__main__":
     trainval()

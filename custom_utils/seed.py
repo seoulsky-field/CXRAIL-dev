3606a24fc1feb6c7f745414afd9c338e4076ca4d
Kyoungmin Jeon <83350060+seoulsky-field@users.noreply.github.com>
2023-02-07 13:49:22 +0900

    Hotfix: Revise seed_everything function (#92)

--- custom_utils/seed.py
+++ custom_utils/seed.py
@@ -14,6 +14,6 @@ def seed_everything(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
 
     print(f"SUCCESS {seed} SEED FIXING")

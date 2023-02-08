be6f31d73a2d2103138011ec73577ff4c1502b25
Kyoungmin Jeon <83350060+seoulsky-field@users.noreply.github.com>
2023-02-08 11:01:32 +0900

    Hotfix: Revise seed fixing in multi-gpu (#93)

--- custom_utils/seed.py
+++ custom_utils/seed.py
@@ -12,7 +12,7 @@ def seed_everything(seed: int = 42):
     os.environ["PYTHONHASHSEED"] = str(seed)
 
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
